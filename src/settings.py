55f86e7ef9f75cb13dcb7cce57d2088b854cd8f1
Анатолий Чудаков <anatoliy.chudakov020@gmail.com>
2021-09-29 18:35:53 +0300

    add_rnn_train_settings

--- src/settings.py
+++ src/settings.py
@@ -5,3 +5,25 @@ logreg_params = dict(multi_class='ovr',
                      n_jobs=-1,
                      penalty='l2',
                      C=0.5)
+
+rnn_params = dict(
+    # Пути к данным
+    df="../coleridgeinitiative-show-us-the-data/train.csv",
+    vectorizer_file="vectorizer.json",
+    model_state_file="model.pth",
+    save_dir="../models",
+    # Гиперпараметры архитектуры нейросети
+    char_embedding_size=300,
+    rnn_hidden_size=64,
+    # Гиперпараметры тренировки нейросети
+    num_epochs=300,
+    learning_rate=1e-2,
+    batch_size=32,
+    seed=1337,
+    early_stopping_criteria=5,
+    # Runtime hyper parameter
+    cuda=False,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepaths_to_save_dir=True,
+)
